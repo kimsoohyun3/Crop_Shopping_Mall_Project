195c3c1d6b68d74b7c67f67f6f368b1ef12291dc
kimsoohyun <frodo9608@naver.com>
2022-09-19 20:20:34 +0900

    test : 상품 등록 테스트 코드 추가

--- src/main/java/project/repository/ItemImgRepository.java
+++ src/main/java/project/repository/ItemImgRepository.java
@@ -3,5 +3,8 @@ package project.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.entity.ItemImg;
 
+import java.util.List;
+
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
 }
